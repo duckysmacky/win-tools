431888d44751650140b9d15a6e3aab85c9d8f4dd
Ducky <kriperoka@gmail.com>
2024-04-01 19:28:51 +0300

    simple ls command

--- src/ls/ls.c
+++ src/ls/ls.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+#include <dirent.h>
 
 int main(int argc, char const *argv[])
 {
+    DIR *dir;
+    struct dirent *dEntry;
+
+    if (argc > 1) dir = opendir(argv[1]);
+    else dir = opendir("./");
+
+    if (dir == NULL)
+    {
+        printf("Error opening directory!\n");
+        return 1;
+    }
+
+    while ((dEntry = readdir(dir)) != NULL)
+    {
+        printf("%s\n", dEntry -> d_name);
+    }
+
+    if (closedir(dir) == -1)
+    {
+        printf("Error opening directory!\n");
+        return 1;
+    }
+
     return 0;
 }
