10658ea87242fa9c25e1dc6d0556cd2ddc3ae9c2
Ducky <kriperoka@gmail.com>
2024-04-11 16:40:06 +0300

    added getopt for grep

--- Makefile
+++ Makefile
@@ -1,14 +1,21 @@
+# TODO - make better makefile
+ARGS = -Wall
+OBJS := out/obj/arr-utils.o out/obj/str-utils.o
+
 all: util ls touch grep
+
+clean:
+	rm out/bin/*
 
 util:
 	gcc utils/str-utils.c -c -o out/obj/str-utils.o
 	gcc utils/arr-utils.c -c -o out/obj/arr-utils.o
 
 ls:
-	gcc src/ls/ls.c out/obj/str-utils.o out/obj/arr-utils.o -o out/bin/ls -Wall
+	gcc src/ls/ls.c -o out/bin/ls $(OBJS) $(ARGS)
 
 touch:
-	gcc src/touch/touch.c out/obj/arr-utils.o out/obj/str-utils.o -o out/bin/touch -Wall
+	gcc src/touch/touch.c -o out/bin/touch $(OBJS) $(ARGS)
 
 grep:
-	gcc src/grep/grep.c out/obj/str-utils.o -o out/bin/grep -Wall
+	gcc src/grep/grep.c -o out/bin/grep $(OBJS) $(ARGS)
--- src/grep/grep.c
+++ src/grep/grep.c
@@ -2,11 +2,86 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
 
 #include "../../utils/str-utils.h"
 #include "../../utils/colors.h"
 
-// TODO - options support
+#define USAGEMSG "Usage: grep [OPTIONS] <FILE> <PATTERN>\n"
+
+void readDir(const char *path, const char *pattern);
+void readFile(const char *path, const char *pattern);
+
+int main(int argc, char const *argv[])
+{
+    char *path = ".";
+    int opt;
+    // TODO - implement all options
+    // read options
+    while ((opt = getopt(argc, argv, "lfh")) != -1)
+    {
+        switch (opt)
+        {
+            case 'h': break; // help
+            case 'c': break; // count how many types pattern appears
+            case 'i': break; // case-insensitive
+            case 'l': break; // show line number
+            case 'f': break; // show full line
+            case 'p': break; // specify path (probably will remove)
+            case 'w': break; // seperate word (only whole)
+            case 'r':
+            case 'R': break; // look in current dir and all subdirs
+            case 'F': break; // find all lines with pattern (alias - fgrep)
+            default:
+                printf("Error: unknown option %c!\n", optopt);
+                printf(USAGEMSG);
+                return 1;
+        }
+    }
+
+    // count all non-options
+    int noptc[3];
+    for(int i = 0; optind < argc; optind++)
+    {
+        printf("%d: %s\n", optind, argv[optind]);
+    } 
+
+    const char *pattern = argv[optind];
+    
+    // check for all required args
+    if (optind < 2)
+    {
+        printf("Error: missing arguments!");
+        printf(USAGEMSG);
+        return 1;
+    }
+
+    if (fopen(path, "r"))
+    {
+        readFile(path, pattern);
+    }
+    else if (opendir(path))
+    {
+        if (stroccr(path, '/') > 0 || stroccr(path, '\\') > 0) // if there are slashes
+        {
+            readDir(path, pattern);
+        }
+        else
+        {
+            char *dpath = malloc(sizeof(strlen(path) + 2) * sizeof(char));
+            sprintf(dpath, "%s/", path);
+            readDir(dpath, pattern);
+            free(dpath);
+        }
+    }
+    else
+    {
+        printf("error: file doesn't exist!");
+        return 1;
+    }
+    
+    return 0;
+}
 
 void readFile(const char *path, const char *pattern)
 {
@@ -86,42 +161,4 @@ void readDir(const char *path, const char *pattern)
         free(ename);
     }
     return;
-}
-
-int main(int argc, char const *argv[])
-{
-    if (argc < 3)
-    {
-        printf("missing arguments! usage: grep <PATTERN> <FILE>");
-        return 1;
-    }
-
-    const char *pattern = argv[1];
-    const char *path = argv[2];
-
-    if (fopen(path, "r"))
-    {
-        readFile(path, pattern);
-    }
-    else if (opendir(path))
-    {
-        if (stroccr(path, '/') > 0 || stroccr(path, '\\') > 0) // if there are slashes
-        {
-            readDir(path, pattern);
-        }
-        else
-        {
-            char *dpath = malloc(sizeof(strlen(path) + 2) * sizeof(char));
-            sprintf(dpath, "%s/", path);
-            readDir(dpath, pattern);
-            free(dpath);
-        }
-    }
-    else
-    {
-        printf("error: file doesn't exist!");
-        return 1;
-    }
-    
-    return 0;
 }
