4e48ef99bf6f0958dfad34ae62a48af499b9eb87
Ducky <kriperoka@gmail.com>
2024-04-12 19:15:43 +0300

    bool data and -n fix

---
+++ README.md
@@ -0,0 +1,20 @@
+# A collection of useful command line tools
+
+Some of these tools are just their Linux equivalent, as they are not there in CMD or Powershell by default (with exceptions)
+
+## Features
+All binaries be found in `out/bin/` and can be added to `PATH` to be used anywhere
+
+All currently avaliable tools and features are listed below (as well as future plans)
+
+### Linux commands
+- [X] ls
+  - [X] colors
+  - [ ] options
+- [X] touch
+- [X] grep
+  - [ ] options
+
+### Other tools
+- [ ] Port scanner
+- [ ] ASCII decoration generator
---
+++ src/commands/touch/touch.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+#include "../../headers/arr-utils.h"
+#include "../../headers/str-utils.h"
+
+int main(int argc, char const *argv[])
+{
+    FILE *file;
+    if (argc < 2)
+    {
+        printf("missing arguments! usage: touch <FILE>");
+        return 1;
+    }
+
+    const char *fpath = argv[1];
+    int sloccr = stroccr(fpath, '/') + stroccr(fpath, '\\'); // total occurences of slashes
+    int slpos[sloccr], j = 0; // indexes of slashes
+    for (int i = 0; i < strlen(fpath); i++)
+    {
+        if (fpath[i] == '/' || fpath[i] == '\\')
+        {
+            slpos[j] = i; // add the index of a slash
+            j++;
+        }
+    }
+
+    int slposLen = arrlen(slpos);
+    if (slposLen > 0)
+    {
+        // checking if full path exists, if not creating each dir
+        for (int i = 0; i < slposLen; i++) // for each index of slash
+        {
+            int slidx = slpos[i]; // slash index
+            char *dirpath = malloc((slidx + 1) * sizeof(char));
+            for (int j = 0; j < slidx; j++) // write dir name (until a slash)
+            {
+                dirpath[j] = fpath[j];
+            }
+            dirpath[slidx] = '\0';
+
+            if (opendir(dirpath) == NULL) // if dir doesnt exist 
+            {
+                mkdir(dirpath);
+            }
+
+            free(dirpath);
+        }
+    }
+
+    // create the file itself
+    file = fopen(fpath, "w");
+    fclose(file);
+
+    return 0;
+}
