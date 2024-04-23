7cedb5ed0ff87eb0a91a046e7eeb011b1faa3f89
Ducky <kriperoka@gmail.com>
2024-04-23 23:43:13 +0300

    ls recursive search

--- src/commands/ls/ls.c
+++ src/commands/ls/ls.c
@@ -1,52 +1,116 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
+#include "ls.h"
 
-#include "../../include/arr-utils.h"
-#include "../../include/colors.h"
+char *NAME_EXEPTIONS[] = {
+    "Makefile"
+};
 
 int main(int argc, char const *argv[])
 {
-    char *NAME_EXEPTIONS[] = {
-        "Makefile"
-    };
-    struct dirent* dEntry;
-    DIR *dir = argc > 1 ? opendir(argv[1]) : opendir("./");
+    Opts opts = LsOpts_default;
 
-    if (dir == NULL)
+    // read options
+    int opt;
+    while ((opt = getopt(argc, (char * const*) argv, OPTION_FLAGS)) != -1)
     {
-        printf("Error opening directory!\n");
-        return 1;
+        switch (opt)
+        {
+            // case 'h': break; // TODO - add help
+            case 'l': opts.l = true; break;
+            case 'a': opts.a = true; break;
+            case 't': opts.t = true; break;
+            case 'r': opts.r = true; break;
+            case 'i': opts.i = true; break;
+            case 'g': opts.g = true; break;
+            case 'h': opts.h = true; break;
+            case 'd': opts.d = true; break;
+            case 's': opts.s = true; break;
+            case 'A': opts.A = true; break;
+            case 'R': opts.R = true; break;
+            default:
+                printf("error: unknown option %c!\n", optopt);
+                printf("do %s --help for help\n", argv[0]);
+                printf(MSG_USAGE);
+                return 1;
+        }
     }
 
-    // TODO - make files print in order
+    // holds specified dir path, else current
+    const char *dirpath = argv[optind] ? argv[optind] : "./";
+
+    if (opts.R)
+    {
+        prtblue("%s:\n", dirpath);
+        listDir(dirpath, &opts);
+        readDir(dirpath, &opts);
+    }
+    else
+    {
+        listDir(dirpath, &opts);
+    }
+
+    return 0;
+}
+
+void listDir(const char *path, Opts *opts)
+{
+    struct dirent *dEntry;
+    DIR *dir = opendir(path);
+
     while ((dEntry = readdir(dir)))
     {
-        char* name = malloc((strlen(dEntry -> d_name) + 1) * sizeof(char)); // file name
+        char *name = malloc((strlen(dEntry -> d_name) + 1) * sizeof(char)); // file name
         strcpy(name, dEntry -> d_name);
         // ignore . and .. files
-        if (!strcmp(name, ".") || !strcmp(name, "..")) {
+        if ((!strcmp(name, ".") || !strcmp(name, "..")) && !opts->A) {
             continue;
         }
 
-        const char* ext = strrchr(name, '.'); // holds file extention
-        if (ext == NULL && strinarr(NAME_EXEPTIONS, sizeof(NAME_EXEPTIONS), name))
+        const char *ext = strrchr(name, '.'); // holds file extention
+        if (ext == NULL && strinarr(NAME_EXEPTIONS, sizeof(NAME_EXEPTIONS), name)) // if theres no extention (dir)
         { 
-            prtgreen("%s/ ", name);
+            prtblue("%s/ ", name);
         }
-        else if (((int)(ext - name)) == 0) // if index of the extention is 0
+        else if (((int)(ext - name)) == 0) // if index of the extention is 0 (.files and hidden files)
         { 
-            prtblue("%s ", name);
+            prtcyan("%s ", name);
         }
-        else
+        else // all other files
         { 
-            printf("%s ", name);
+            prtgreen("%s ", name);
         }
 
         free(name);
     }
 
+    puts("\n");
     closedir(dir);
-    return 0;
+    return;
+}
+
+void readDir(const char *path, Opts *opts)
+{
+    struct dirent *dEntry;
+    DIR *dir = opendir(path);
+
+    while ((dEntry = readdir(dir)))
+    {
+        char *ename = dEntry->d_name; // entry name
+        if (!strcmp(ename, ".") || !strcmp(ename, "..")) continue;
+
+        char *dpath = malloc(sizeof(path) + sizeof(ename) + sizeof(char) * 2); // full path to dir
+        sprintf(dpath, "%s/%s", path, ename);
+
+        // try to open entry item as dir
+        if (opendir(dpath))
+        {
+            prtblue("%s:\n", dpath);
+            listDir(dpath, opts);
+            readDir(dpath, opts);
+        }
+
+        free(dpath);
+    }
+
+    closedir(dir);
+    return;
 }
---
+++ src/commands/ls/ls.h
@@ -0,0 +1,66 @@
+#ifndef LS_H
+#define LS_H
+
+// Includes
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#include "../../include/arr-utils.h"
+#include "../../include/colors.h"
+
+// Data
+
+#define true 1
+#define false 0
+#define bool unsigned char
+
+// Ls Options
+struct LsOpts {
+    bool l; // show detailed info about files and dirs
+    bool a; // show all files (including hidden)
+    bool t; // sort by modify time
+    bool r; // reverse sort
+    bool i; // show index of files
+    bool g; // show group ownership (?)
+    bool h; // print file sizes in human-readable format (e.g. k, mb, gb)
+    bool d; // list dirs instead of files
+    bool s; // sort by size
+    bool A; // show ALL files (including hidden, . and ..)
+    bool R; // show recursively (all subdirs)
+} LsOpts_default = {
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+};
+typedef struct LsOpts Opts, *POpts;
+
+// Strings
+
+#define MSG_USAGE "Usage: ls [OPTIONS]\n"
+#define OPTION_FLAGS "latrighdsAR"
+
+// Functions
+
+/*
+    List items in dir
+*/
+void listDir(const char *path, Opts *opts);
+
+/*
+    Read dir and find subdirs, then list them if found
+*/
+void readDir(const char *path, Opts *opts);
+
+#endif // LS_H
