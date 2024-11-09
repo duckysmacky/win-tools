8415713efe312c2cc8bdc58adfc705d753d46915
DuckySmacky <kriperoka@gmail.com>
2024-11-09 15:05:50 +0300

    Add colored std::cout implementtation (utils::ccout)

--- src/utils/colors.h
+++ src/utils/colors.h
@@ -1,20 +1,7 @@
-#ifndef COLORS_H
-#define COLORS_H
+#ifndef UTILS_COLOR_H
+#define UTILS_COLOR_H
 
-#ifdef __cplusplus
-namespace utils::colors
-{
-	constexpr auto RESET = "\033[0m";
-	constexpr auto BLACK = "\033[0;30m";
-	constexpr auto RED = "\033[0;31m";
-	constexpr auto GREEN = "\033[0;32m";
-	constexpr auto YELLOW = "\033[0;33m";
-	constexpr auto BLUE = "\033[0;34m";
-	constexpr auto PURPLE = "\033[0;35m";
-	constexpr auto CYAN = "\033[0;36m";
-	constexpr auto WHITE = "\033[0;37m";
-}
-#endif
+#include <iostream>
 
 // Color values
 #define COLOR_RESET "\033[0m"
@@ -27,14 +14,44 @@ namespace utils::colors
 #define COLOR_CYAN "\033[0;36m"
 #define COLOR_WHITE "\033[0;37m"
 
-// Print color macros
-#define printBlack(msg, ...) printf("%s" msg "%s", COLOR_BLACK, ##__VA_ARGS__, COLOR_RESET);
-#define printRed(msg, ...) printf("%s" msg "%s", COLOR_RED, ##__VA_ARGS__, COLOR_RESET);
-#define printGreen(msg, ...) printf("%s" msg "%s", COLOR_GREEN, ##__VA_ARGS__, COLOR_RESET);
-#define printYellow(msg, ...) printf("%s" msg "%s", COLOR_YELLOW, ##__VA_ARGS__, COLOR_RESET);
-#define printBlue(msg, ...) printf("%s" msg "%s", COLOR_BLUE, ##__VA_ARGS__, COLOR_RESET);
-#define printPurple(msg, ...) printf("%s" msg "%s", COLOR_PURPLE, ##__VA_ARGS__, COLOR_RESET);
-#define printCyan(msg, ...) printf("%s" msg "%s", COLOR_CYAN, ##__VA_ARGS__, COLOR_RESET);
-#define printWhite(msg, ...) printf("%s" msg "%s", COLOR_WHITE, ##__VA_ARGS__, COLOR_RESET);
+namespace colors
+{
+	constexpr auto RESET = COLOR_RESET;
+	constexpr auto BLACK = COLOR_BLACK;
+	constexpr auto RED = COLOR_RED;
+	constexpr auto GREEN = COLOR_GREEN;
+	constexpr auto YELLOW = COLOR_YELLOW;
+	constexpr auto BLUE = COLOR_BLUE;
+	constexpr auto PURPLE = COLOR_PURPLE;
+	constexpr auto CYAN = COLOR_CYAN;
+	constexpr auto WHITE = COLOR_WHITE;
+}
 
-#endif // !COLORS_H
+namespace utils
+{
+	// Colored std::cout alternative
+	class ccout
+	{
+	public:
+	    inline ccout(const std::string& color)
+	    {
+	        std::cout << color;
+	    }
+	
+	    template <class T>
+	    inline ccout& operator<<(const T& input)
+	    {
+	        std::cout << input;
+	        return *this;
+	    }
+	
+	    inline ~ccout()
+	    {
+	        std::cout << colors::RESET << std::endl;
+	    }
+	};
+
+
+}
+
+#endif // !UTILS_COLOR_H
