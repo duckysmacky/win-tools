0de98e86daf704623f92f3f72d0241c8cebd2ad0
DuckySmacky <kriperoka@gmail.com>
2024-11-14 11:45:10 +0300

    Remove depricated string utils, move to utils namespace

--- src/utils/string-utils.cpp
+++ src/utils/string-utils.cpp
@@ -4,14 +4,16 @@
 
 #include <string.h>
 
-#include "array-utils.h"
-
 namespace utils
 {
     bool isNumber(const std::string& str)
     {
         std::string::const_iterator iter = str.begin();
-        while (iter != str.end() && isdigit(*iter)) ++iter;
+        while (iter != str.end())
+        {
+            if (!isdigit(*iter)) return false;
+            ++iter;
+        }
         return !str.empty() && iter == str.end();
     }
 
@@ -34,90 +36,39 @@ namespace utils
 
         return result;
     }
-}
 
-
-char* joinStrings(char *str1, char *str2)
-{
-    char *str = (char*) malloc(sizeof(str1) + sizeof(str2));
-
-    strcpy_s(str, sizeof(str1), str1);
-    strcat_s(str, sizeof(str2), str2);
-
-    return str;
-}
-
-int findChar(int *buff, char *str, char c)
-{
-    int j = 0;
-    for (int i = 0; i < strlen(str); i++)
+    int countCharOccurences(const char* str, const char c)
     {
-        if (str[i] == c)
-        {
-            buff[j] = i;
-            j++;
+        int i;
+        for (i = 0; str[i]; str[i] == c ? i++ : *str++);
+        return i;
+    }
+
+    char* replaceString(char* str, size_t size, const char* substr, const char* newstr)
+    {
+        char* p = strstr(str, substr); // pointer to the substr in str
+        if (p == NULL) return NULL;
+
+        // check if size is enough
+        if (size < strlen(str) - strlen(substr) + (strlen(newstr)) + 1) {
+            printf("Buffer size is too small!\n");
+            return NULL;
         }
+
+        // move
+        memmove(
+            p + strlen(newstr), // to
+            p + strlen(substr), // from
+            strlen(p) - strlen(substr) + 1 // size
+        );
+
+        memcpy(p, newstr, strlen(newstr));
+        return p + strlen(newstr);
     }
 
-    if (sizeof(buff) == 0)
+    char* getSuffix(char* str, int n)
     {
-        return 1;
+        size_t len = strlen(str);
+        return (char*)str + len - n;
     }
-
-    return 0;
-}
-
-int countCharOccurences(const char *str, const char c)
-{
-    int i;
-    for (i = 0; str[i]; str[i] == c ? i++ : *str++);
-    return i;
-}
-
-char* replaceString(char *str, size_t size, const char *substr, const char *newstr)
-{
-    char *p = strstr(str, substr); // pointer to the substr in str
-    if (p == NULL) return NULL;
-
-    // check if size is enough
-    if (size < strlen(str) - strlen(substr) + (strlen(newstr)) + 1) {
-        printf("Buffer size is too small!\n");
-        return NULL;
-    }
-
-    // move
-    memmove(
-        p + strlen(newstr), // to
-        p + strlen(substr), // from
-        strlen(p) - strlen(substr) + 1 // size
-    );
-
-    memcpy(p, newstr, strlen(newstr));
-    return p + strlen(newstr);
-}
-
-void stringToLower(char *buff, char *str)
-{
-    size_t l = strlen(buff);
-    for (int i = 0; i < l; i++)
-    {
-        buff[i] = tolower(str[i]);
-    }
-    buff[l] = '\0';
-}
-
-void stringToUpper(char *buff, char *str)
-{
-    size_t l = strlen(buff);
-    for (int i = 0; i < l; i++)
-    {
-        buff[i] = toupper(str[i]);
-    }
-    buff[l] = '\0';
-}
-
-char* getSuffix(char *str, int n)
-{
-    size_t len = strlen(str);
-    return (char *) str + len - n;
 }
--- src/utils/string-utils.h
+++ src/utils/string-utils.h
@@ -8,32 +8,12 @@ namespace utils
     bool isNumber(const std::string &str);
     std::string toLowercase(const std::string& str);
     std::string toUppercase(const std::string& str);
-}
-
-#ifdef __cplusplus
-extern "C" {
-#endif 
-    /*
-        DOESNT WORK AS INTENDED
-        Adds characters to the end of the string
-        @param str string to append to
-        @param c char to append
-        @return status code
-    */
-    char* joinStrings(char* str1, char* str2);
     /*
         Searches the string for characters
         @param buff location to write the found indexes
         @param str string to search in
         @param c char to search
         @return true if found any
-    */
-    int findChar(int* buff, char* str, char c);
-    /*
-        Counts occurences of a sting in string
-        @param str source string
-        @param c string to search in source
-        @return occurences of c in str
     */
     int countCharOccurences(const char* str, const char c);
     /*
@@ -46,26 +26,13 @@ extern "C" {
     */
     char* replaceString(char* str, size_t size, const char* substr, const char* newstr);
     /*
-        Converts all characters in a string to lowercase
-        @param buff buffer to write converted string
-        @param str original string
-    */
-    void stringToLower(char* buff, char* str);
-    /*
-        Converts all characters in a string to uppercase
-        @param buff buffer to write converted string
-        @param str original string
-    */
-    void stringToUpper(char* buff, char* str);
-    /*
         Get the last n characters from the string
         @param str string
         @param n amount of characters to get
         @return last n characters of string
     */
     char* getSuffix(char* str, int n);
-#ifdef __cplusplus
+
 }
-#endif
 
 #endif
