0f22c9da649c9d8bbd035ec6c02e1872b181640d
DuckySmacky <kriperoka@gmail.com>
2025-01-03 02:52:42 +0300

    Migrate Ls to use new argument parsing system, improve Ls in general, fix Ls bugs

--- src/ls/ls.h
+++ src/ls/ls.h
@@ -4,47 +4,18 @@
 #define NAME "Ls"
 #define VERSION "1.0.0"
 
-#define MESSAGE_VERSION NAME " v" VERSION
-#define MESSAGE_USAGE "Usage: ls [OPTIONS]... <DIRECTORY>\n"
-#define MESSAGE_HELP "TODO"
-
-#include "colors.h"
-
-#define OPTIONS_FLAGS "VhlaAdR123456789"
-
-struct Options
-{
-    bool longFormat; // l
-    bool showHidden; // a
-    bool showAll; // A
-    bool listDirs; // d
-    bool recursive; // R
-    int rows; // [1-9]
-    // TODO bool showIndex; // i
-    // TODO bool showGroup; // g
-    // TODO bool humanSize; // H (e.g. k, mb, gb)
-    // TODO bool sortByModifyTime; // t
-    // TODO bool sortBySize; // s
-    // TODO bool reverseSort; // r
-
-    Options()
-    {
-        longFormat = false;
-        showHidden = false;
-        showAll = false;
-        listDirs = false;
-        recursive = false;
-        rows = 0;
-    }
-};
-
 // Settings
+
+#include "utils.h" // colors
+
+// I don't even remember how, why or what for. No questions here
+#define MAGIC_WIDTH 11
 
 #define LIST_NAME_EXEPTIONS { "Makefile" }
 
-#define FILE_COLOR_DIRECTORY COLOR_BLUE
-#define FILE_COLOR_HIDDEN COLOR_CYAN
-#define FILE_COLOR_EXECUTABLE COLOR_YELLOW
-#define FILE_COLOR_OTHER COLOR_GREEN
+#define COLOR_FILE_DIRECTORY COLOR_BLUE
+#define COLOR_FILE_HIDDEN COLOR_CYAN
+#define COLOR_FILE_EXECUTABLE COLOR_YELLOW
+#define COLOR_FILE_OTHER COLOR_GREEN
 
 #endif // !COMMANDS_LS_H
